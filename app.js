a996c0e885ac78409da88c2a9adaf4a1440321ca
gregtandiono <gregtandiono@gmail.com>
2015-08-23 14:08:31 -0700

    added react client

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-"use strict"
+/**
+ * app.js
+ * @flow
+ */
+
+"use strict";
+
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+
+// Generate Fake Users with faker
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Enable CORS middleware
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get("/", function(req, res) {
+  res.sendFile("public/index.html")
+});
+
+var server = app.listen(1337, function() {
+  var port = server.address().port;
+  console.log('App server is running on port:', port);
+});
+
+module.exports = app;
--- producer.js
+++ producer.js
@@ -4,7 +4,7 @@ const sock = zmq.socket("push")
 sock.bindSync('tcp://127.0.0.1:3000');
 console.log('Producer bound to port 3000');
 
-setInterval(function(){
-  console.log('sending work');
-  sock.send('some work');
-}, 500);
+// setInterval(function(){
+//   console.log('sending work');
+//   sock.send('some work');
+// }, 500);
