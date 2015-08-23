eec2282675198df48c6c481dca22ec6df19a3344
gregtandiono <gregtandiono@gmail.com>
2015-08-23 12:43:59 -0700

    initial commit

---
+++ worker.js
@@ -0,0 +1,9 @@
+import zmq from "zmq"
+const sock = zmq.socket("pull")
+
+sock.connect('tcp://127.0.0.1:3000');
+console.log('Worker connected to port 3000');
+
+sock.on('message', function(msg){
+  console.log('work: %s', msg.toString());
+});
