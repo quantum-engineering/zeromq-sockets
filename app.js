aece00b56b3c8b4dc6d87dd60eb9346fa24375ec
gregtandiono <gregtandiono@gmail.com>
2015-08-23 20:43:09 -0700

    created new channel publisher and subscriber to /chat

--- app.js
+++ app.js
@@ -27,26 +27,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Enable CORS middleware
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
 });
 
 app.get("/", function(req, res) {
-  res.sendFile("public/index.html")
+	res.sendFile("public/index.html")
 });
 
 io.on("connection", function(socket) {
-  console.info("client connection established")
-  socket.on("chat message", function(msg) {
-    console.info("message:", msg)
-  })
+	console.info("client connection established")
+	socket.on("chat message", function(msg) {
+		console.info("message:", msg)
+	})
 });
+
+var chat = io
+	.of("/chat")
+	.on("connection", (socket) => {
+		console.info("client connected to /chat")
+		socket.emit("intro", {
+			message: "Welcome to Chat Channel Page with socket"
+		})
+	})
+	.on("chat message created", (msg) => {
+		console.info("message:", msg)
+	})
 
 
 server.listen(1337, function() {
-  var port = server.address().port;
-  console.info("App server is running on port:", port)
+	var port = server.address().port;
+	console.info("App server is running on port:", port)
 })
 
 module.exports = app;
--- public/javascripts/actions/ChatActions.js
+++ public/javascripts/actions/ChatActions.js
@@ -7,25 +7,34 @@ import AppDispatcher from "../dispatcher/AppDispatcher"
 import {ChatConstants} from "../constants/ChatConstants"
 import request from "superagent"
 import io from "socket.io-client"
-const socket = io()
+
+
+/**
+ * Current Host using window location
+ * this is a hack obviously, because we need a
+ * separate host to serve the socket server side
+ */
+
+const currentLocation = window.location.origin
+const socket = io(`${currentLocation}/chat`)
 
 export const ChatActions = {
 	get(payload) {
-    payload = payload ? payload : null
-    console.info("ChatActions GET Triggered", payload)
+		payload = payload ? payload : null
+		console.info("ChatActions GET Triggered", payload)
 		AppDispatcher.dispatch({
 			actionType: ChatConstants.MESSAGE_LOAD,
-      message: payload,
+			message: payload,
 			loading: true
 		})
 	},
 	create(payload) {
-    socket.emit("chat message", payload)
+		socket.emit("chat message", payload)
 		AppDispatcher.dispatch({
 			actionType: ChatConstants.MESSAGE_CREATE,
-      message: payload
+			message: payload
 		})
-    // this.get(payload)
+		// this.get(payload)
 	}
 
 }
